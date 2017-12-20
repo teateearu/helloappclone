3f5232d3db814364bdf69d9386e3defb620e6a65
“Tea <teateearu@gmail.com>
2017-12-20 15:04:51 +0100

    Add bootstrap and dropdown menu

--- src/hosts/HostsContainer.js
+++ src/hosts/HostsContainer.js
@@ -1,60 +1,28 @@
-// import React, { PureComponent } from 'react';
-// import AutoComplete from 'material-ui/AutoComplete';
-// import { connect } from 'react-redux'
-// import './index.css'
-// // import hosts from '../reducers/hosts'
-//
-// /**
-//  * `AutoComplete` search text can be implemented as a controlled value,
-//  * where `searchText` is handled by state in the parent component.
-//  * This value is reset with the `onNewRequest` callback.
-//  */
-//  const hosts = [
-//    'Lara / Meeting1',
-//    'Robin / Meeting2',
-//    'Daniil / Meeting3',
-//    'Ha / Meeting4',
-//    'Tea / Meeting5',
-//    'Gabrijela / Meeting6'
-//  ];
-//
-// class HostsContainer extends PureComponent {
-//   state = {
-//     searchText: '',
-//   };
-//
-//   handleUpdateInput = (searchText) => {
-//     this.setState({
-//       searchText: searchText,
-//     });
-//   };
-//
-//   handleNewRequest = () => {
-//     this.setState({
-//       searchText: '',
-//     });
-//   };
-//
-//   render() {
-//     return (
-//       <div className="Host">
-//         <AutoComplete
-//           className="Lala"
-//           hintText="Search host/meeting name"
-//           searchText={this.state.searchText}
-//           onUpdateInput={this.handleUpdateInput}
-//           onNewRequest={this.handleNewRequest}
-//           dataSource={hosts}
-//           filter={(searchText, key) => (key.indexOf(searchText) !== -1)}
-//           openOnFocus={true}
-//         />
-//       </div>
-//     );
-//   }
-// }
-//
-// const mapStateToProps = ({ hosts }) => ({
-//   hosts
-// })
-//
-// export default connect(mapStateToProps)(HostsContainer)
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux'
+import './index.css'
+import { ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap'
+
+class HostsContainer extends PureComponent {
+
+  render() {
+    return (
+      <div>
+      <ButtonGroup>
+        <DropdownButton title="Choose your host" id="bg-nested-dropdown">
+          <MenuItem eventKey="1">Lara</MenuItem>
+          <MenuItem eventKey="2">Robin</MenuItem>
+          <MenuItem eventKey="3">Daniil</MenuItem>
+          <MenuItem eventKey="4">Ha</MenuItem>
+          <MenuItem eventKey="5">Gabrijela</MenuItem>
+          <MenuItem eventKey="6">Tea</MenuItem>
+        </DropdownButton>
+      </ButtonGroup>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ hosts }) => ({ hosts })
+
+export default connect(mapStateToProps)(HostsContainer)
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import store from './store'
 
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
