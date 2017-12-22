3ca5caa5abb63e25ebe0ec766bbf22671c13bb9e
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-12-22 14:17:23 +0100

    Styling button and gradient background

--- src/hosts/HostsContainer.js
+++ src/hosts/HostsContainer.js
@@ -39,7 +39,7 @@ class HostsContainer extends PureComponent {
           <DropdownButton className="dropdownbutton" title="Choose your host" id="bg-dropdown" >
             { this.props.hosts.map((host,index) => <MenuItem className="menuitem" key={ index } onSelect={this.updateShowAlert.bind(this, host)}> { host } </MenuItem>) }
           </DropdownButton>
-          <HostsDialog visible={this.state.showAlert} host={this.state.host} hideAlert={this.hideAlert.bind(this)} sendEmail={this.sendEmail.bind(this)}/>
+          <HostsDialog className="alert"visible={this.state.showAlert} host={this.state.host} hideAlert={this.hideAlert.bind(this)} sendEmail={this.sendEmail.bind(this)}/>
         </div>
       </div>
     );
