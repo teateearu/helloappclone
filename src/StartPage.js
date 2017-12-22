4e601f7850073fcbd0590d0b3f6bdbf71dbae338
laraanna <lara@laraannawagner.com>
2017-12-22 15:48:52 +0100

    Set up styling

--- src/StartPage.js
+++ src/StartPage.js
@@ -1,25 +1,30 @@
-import React, { PureComponent } from 'react'
-import { push } from 'react-router-redux'
-import { connect } from 'react-redux'
-import {Button, Jumbotron} from 'react-bootstrap'
-import './StartPage.css'
+import React, { PureComponent } from "react";
+import { push } from "react-router-redux";
+import { connect } from "react-redux";
+import { Button, Jumbotron } from "react-bootstrap";
+import "./StartPage.css";
 
-class StartPage extends PureComponent{
-  startPage = () =>
-  {
-    this.props.push('/camera')
-  }
+class StartPage extends PureComponent {
+  startPage = () => {
+    this.props.push("/camera");
+  };
 
   render() {
-    return(
+    return (
       <div className="StartPage">
         <div>
           <h1> Welcome to Lightbase </h1>
         </div>
-        <Button className="Start" bsSize="large" onClick={this.startPage.bind(this)}>START</Button>
+        <Button
+          className="Start"
+          bsSize="large"
+          onClick={this.startPage.bind(this)}
+        >
+          Say hello to our camera
+        </Button>
       </div>
-    )
+    );
   }
 }
 
-export default connect(null, { push })(StartPage)
+export default connect(null, { push })(StartPage);
--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -1,16 +1,18 @@
-import React, { PureComponent } from 'react'
-import HostsContainer from './hosts/HostsContainer'
+import React, { PureComponent } from "react";
+import HostsContainer from "./hosts/HostsContainer";
+import "./WelcomeMessage.css";
 
-class WelcomeMessage extends PureComponent{
-
+class WelcomeMessage extends PureComponent {
   render() {
-    const {message} = this.props.match.params
-    return(
+    const { message } = this.props.match.params;
+    return (
       <div className="WelcomeMessage">
-      <p>{message}</p>
+        <div>
+          <h1>{message}</h1>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default WelcomeMessage
+export default WelcomeMessage;
