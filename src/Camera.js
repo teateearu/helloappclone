ac57ff503d2cd27dc4c607db210d52c0c0ef2a43
Ha-Doan <doanthanhha@gmail.com>
2018-01-02 13:51:47 +0100

    Immediate redirection after recognition.

--- src/Camera.js
+++ src/Camera.js
@@ -16,28 +16,21 @@ class Camera extends PureComponent {
 
   wait = ms => new Promise(r => setTimeout(r, ms))
 
-  repeat =
-      (ms, func) => {
+  repeat = (ms, func) => {
         var intervalID = 0
         var messageArr = null
         new Promise(
           r => {
-              intervalID = setInterval(func, ms),
-              messageArr = this.props.messageArray
-              if (messageArr.length > 0){
-                if (messageArr[messageArr.length - 1] !==  null)
-                  clearInterval(intervalID)
-                else {
-                  setTimeout(() => {  clearInterval(intervalID)
-                  } , 11000),
-                  this.wait(ms).then(r)
-                }
-              }
+              intervalID = setInterval(func, ms)
+
+              setTimeout(() => {  clearInterval(intervalID)
+              } , 11000)
+              console.log("intervalID")
+
           }
       )}
 
-  takePhoto =
-      () => {
+  takePhoto = () => {
         var video = null
         var w = 0
         var h = 0
@@ -46,17 +39,18 @@ class Camera extends PureComponent {
         var dataURI = null
         new Promise(
           r => {
-                  video = document.querySelector('video'),
-                  w = video.videoWidth * 0.5,
-                  h = video.videoHeight * 0.5,
-                  canvas = document.createElement('canvas'),
+                  video = document.querySelector('video')
+                  if (!video) {return null}
+                  w = video.videoWidth * 0.5
+                  h = video.videoHeight * 0.5
+                  canvas = document.createElement('canvas')
 
-                  canvas.width = w,
-                  canvas.height = h,
-                  ctx = canvas.getContext('2d'),
-                  ctx.drawImage(video, 0, 0, w, h),
+                  canvas.width = w
+                  canvas.height = h
+                  ctx = canvas.getContext('2d')
+                  ctx.drawImage(video, 0, 0, w, h)
 
-                  dataURI = canvas.toDataURL('image/jpeg'),
+                  dataURI = canvas.toDataURL('image/jpeg')
                   this.props.sendPhoto(dataURI)
                   return dataURI
                 }
@@ -79,17 +73,20 @@ class Camera extends PureComponent {
       if (messageArr.length > 0){
         const nullArray = messageArr.filter(element => element.message ===  null)
         if (nullArray.length === 10){
-          this.props.push('/message/nomatch')
-          //setTimeout(function(){window.location.href="/"}, 50000)
+          const message = "No match found. Please notify your host."
+          this.props.push(`/message/nomatch/${message}`)
+          setTimeout(function(){window.location.href="/"}, 50000)
         }
-        else if (messageArr[messageArr.length - 1] !== null){
+        if (messageArr[messageArr.length - 1] !== null){
           const message = messageArr[messageArr.length - 1].message
           this.props.push(`/message/welcome/${message}`)
-          //setTimeout(function(){window.location.href="/"}, 5000)
+          setTimeout(function(){window.location.href="/"}, 10000)
         }
+
       }
         return (
           <div className="Camera">
+          <div><h1>Recognize in {10 - this.props.messageArray.length}</h1></div>
             <Webcam
               audio={false}
               ref={this.setRef}
